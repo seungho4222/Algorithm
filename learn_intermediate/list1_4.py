36ffac2cde285897833dace2341511a331c75dc0
seungho <kials1@naver.com>
2023-07-23 14:53:10 +0900

    230723

---
+++ learn_intermediate/list1_4.py
@@ -0,0 +1,24 @@
+'''N개의 정수가 들어있는 배열에서 이웃한 M개의 합을 계산하는 것은 디지털 필터링의 기초연산이다.
+M개의 합이 가장 큰 경우와 가장 작은 경우의 차이를 출력하는 프로그램을 작성하시오.
+
+[입력]
+첫 줄에 테스트 케이스 개수 T가 주어진다.  ( 1 ≤ T ≤ 50 )
+다음 줄부터 테스트케이스의 첫 줄에 정수의 개수 N과 구간의 개수 M 주어진다. ( 10 ≤ N ≤ 100,  2 ≤ M < N )
+다음 줄에 N개의 정수 ai가 주어진다. ( 1 ≤ a ≤ 10000 )
+
+[출력]
+각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, 답을 출력한다.
+'''
+
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+
+T = int(input())
+for test_case in range(1, T + 1):
+    N, M = map(int, input().split())
+    ai = list(map(int, input().split()))
+    sum_list = [sum(ai[i:i+M]) for i in range(N - M + 1)]
+    
+    print(f'#{test_case} {max(sum_list) - min(sum_list)}')
