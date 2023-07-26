91622af5f4a5a81a93d662364a8806f72c425f85
seungho <kials1@naver.com>
2023-07-26 17:59:00 +0900

    230726

--- learn_intermediate/stack2_4.py
+++ learn_intermediate/stack2_4.py
@@ -21,6 +21,13 @@ sys.stdin = open('input.txt', 'r')
 
 T = int(input())  # 테스트 케이스 개수
 for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for i in range(N)]
+    arr_sum = []
+
+    for i in arr[0]:
+
     
-    
-    ...
+
+        ...
+
