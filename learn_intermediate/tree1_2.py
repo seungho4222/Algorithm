d8c97583580703f16c0fad47a50c4c9b95e1602d
seungho <kials1@naver.com>
2023-08-02 15:23:42 +0900

    230802

--- learn_intermediate/tree1_2.py
+++ learn_intermediate/tree1_2.py
@@ -2,8 +2,22 @@ import sys
 
 sys.stdin = open('input.txt', 'r')
 
+# other
+def make_tree(x):
+    global number
+    if x <= N:
+        make_tree(x*2)
+        tree[x] += number
+        number += 1
+        make_tree(x*2 + 1)
+
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
+    tree = [0] * (N+1)
+    number = 1
+    make_tree(1)
+
+    print(f'#{tc}', tree[1], tree[N//2])
     
