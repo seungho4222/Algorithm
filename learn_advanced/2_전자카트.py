cb64b5d223ddb6207f0cb96f26d686a2f5ae9116
seungho <kials1@naver.com>
2023-08-07 12:21:06 +0900

    230807

--- learn_advanced/2_전자카트.py
+++ learn_advanced/2_전자카트.py
@@ -4,20 +4,36 @@ sys.stdin = open('input.txt', 'r')
 
 # 사무실 출발 - 각 관리구역 1회씩 방문 - 사무실 도착
 def golf_road(x):
-    global min_v
+    global min_battery
     global battery
     global visited
-    visited += [i]
-    for i in range(N):
-        battery += road[x][i]
-        golf_road(i)
-
+    visited += [x]  # 재방문 방지
+    # 관리구역 모두 방문시 사무실로 돌아가고 최소 배터리량과 비교(사무실 돌아가는 배터리량도 계산)
+    if len(visited) == N:
+        battery += road[x][0]
+        if min_battery > battery:
+            min_battery = battery
+        battery -= road[x][0]
+        return
+    # 각 관리구역 돌며 배터리량 추가
+    for i in range(1, N):
+        if i not in visited:
+            battery += road[x][i]
+            golf_road(i)
+            # 다른 루트 찾기 위해 기록 삭제
+            battery -= road[x][i]
+            visited.pop()
+    return
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     road = [list(map(int, input().split())) for _ in range(N)]
-    min_battery = 0
+    # 배터리 사용량 100 이하의 자연수
+    min_battery = 100 * N
     battery = 0
     visited = []
+
+    golf_road(0)
+    print(f'#{tc} {min_battery}')
