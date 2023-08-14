e52661b8df84c34e4fe54803c37db62c22256fab
seungho <kials1@naver.com>
2023-08-14 14:21:00 +0900

    230814

--- learn_advanced/5_전기버스.py
+++ learn_advanced/5_전기버스.py
@@ -6,16 +6,17 @@ sys.stdin = open('input.txt', 'r')
 T = int(input())
 for tc in range(1, T+1):
     N, *M = map(int, input().split())
-    matrix = [0] * (N+1)
-    memo = [0] * (N+1)
+    exchange = 0
+    start = 0
+    while start < N-1:
+        move = 0
+        if start + M[start] >= N-1:
+            break
+        for i in range(start+1,start + M[start]+1):
+            if move <= i + M[i]:
+                move = i + M[i]
+                start = i
+        exchange += 1
 
-    for i in range(1, N+1):
-        cnt = -1
-        for x in M:
-            matrix[i] += x
-            cnt += 1
-            if matrix[i] >= i:
-                break
-        memo[i] = cnt
-    print(memo)
-    # print(f'#{tc} {memo[N]}')
+
+    print(f'#{tc}', exchange)
