e8e9cb06a191250f1d39fff6af8ab4cdaf7216da
miwookkim <minwookkim@naver.com>
2023-08-18 13:33:59 +0900

    230818

--- learn_advanced/7_최소비용.py
+++ learn_advanced/7_최소비용.py
@@ -2,33 +2,35 @@ import sys
 
 sys.stdin = open('input.txt', 'r')
 
-
-def bfs(r, c, ans):
-    global result
-    if r == N-1 and c == N-1:
-        if result > ans:
-            result = ans
-        return
-    if ans > result:
-        return
-    for d in range(2):
-        nr = r + dr[d]
-        nc = c + dc[d]
-        if 0 <= nr < N and 0 <= nc < N:
-            if arr[nr][nc] > arr[r][c]:
-                v = arr[nr][nc] - arr[r][c] + 1
-            else:
-                v = 1
-            bfs(nr, nc, ans+v)
-    return result
+# 다익스트라 알고리즘
+def dijkstra(x, y):
+    # 시작점 스택
+    q = [(x, y)]
+    # 시작 연료 0
+    dp[x][y] = 0
+    while q:
+        r, c = q.pop(0)
+        # 델타 탐색
+        for dr, dc in [[0,1],[1,0],[-1,0],[0,-1]]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < N:
+                # 델타 이동 후 높이 차 계산
+                temp = 0
+                if arr[nr][nc] > arr[r][c]:
+                    temp += arr[nr][nc] - arr[r][c]
+                # 연료 소비량 비교 후 최소값 갱신
+                if dp[nr][nc] > dp[r][c] + temp + 1:
+                    q += [(nr, nc)]
+                    dp[nr][nc] = dp[r][c] + temp + 1
+    # 목적지 연료 소비량 출력
+    return dp[N-1][N-1]
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    dr = [0, 1]
-    dc = [1, 0]
-    result = 1e9
-
-    print(f'#{tc} {bfs(0,0,0)}')
+    # arr의 최소 연료 소비량 배열
+    dp = [[1e9] * N for _ in range(N)]
+    
+    print(f'#{tc} {dijkstra(0,0)}')
