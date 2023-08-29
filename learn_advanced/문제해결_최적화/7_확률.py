ff41f2d25270aad92464033a3cc208f697dc1ae9
seungho <kials1@naver.com>
2023-08-29 20:30:53 +0900

    230829

--- learn_advanced/문제해결_최적화/7_확률.py
+++ learn_advanced/문제해결_최적화/7_확률.py
@@ -1,9 +1,11 @@
+# N자리 자연수 중 각 자릿수에 중복 사용된 숫자 없을 경우 당첨 => 당첨확률은?
+# 각 자릿수 숫자선택시 다음 자리 숫자는 선택지 -1
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    no_dup = 1
     all_case = 10 ** N  # 모든 케이스
-    for i in range(N):  # 중복 없는 케이스
-        no_dup *= 10 - i
+    win_case = 1
+    for i in range(N):  # 각 자리수 중복 제거
+        win_case *= 10 - i
 
-    print(f'#{tc} {no_dup/all_case:.5f}')
+    print(f'#{tc} {win_case/all_case:.5f}')
