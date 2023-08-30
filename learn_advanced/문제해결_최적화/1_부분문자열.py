68cd002eba1d6047f0c21787a5798f49c5b0d644
seungho <kials1@naver.com>
2023-08-31 01:16:12 +0900

    230831

--- learn_advanced/문제해결_최적화/1_부분문자열.py
+++ learn_advanced/문제해결_최적화/1_부분문자열.py
@@ -1,16 +1,29 @@
 T = int(input())
 for tc in range(1, T+1):
-    N, string = input().split()  # 찾아야할 부분문자 번호, 문자열
-    l = len(string)
-    subset = []  # 부분문자열 저장
-    for i in range(1, l + 1):  # 부분문자열 길이 (1 ~ len(s))
-        for j in range(l + 1 - i):  # 각 부분문자열 개수는 길이와 정반대
-            if string[j:j+i] not in subset:
-                subset.append(string[j:j+i])  # 부분문자열 잘라서 저장
-    result = sorted(subset)[int(N)-1]  # 사전순 정렬 후 N번째 부분문자 추출
-    print(f'#{tc} {result[0]} {len(result)}')  # 첫번째 글자와 부분문자열 길이 출력
+    inputs = list(input().split())
+    N, S = int(inputs[0]), inputs[1]  # 찾아야할 부분문자 번호, 문자열
+    A = [''] * len(S)  # 접미어 배열
+    for i in range(len(S)):
+        A[i] = S[i:]
+    A.sort()  # 접미어 배열 사전순 정렬
+    cnt = len(A[0])  # 첫번째 접미어 길이 = 부분문자 수 체크
+    j = 0  # 접미어 순서
+    k = 0  # LCP (Longest Common Prefix) 최장 공통 접두어
+    while N > cnt:  # 아직 찾아야할 부분문자 번호를 못찾았다
+        k = 0  # LCP 초기화
+        j += 1  # 다음 접미어
+        while A[j][k] == A[j-1][k]:  # 이전 접미어와 앞자리부터 같은 글자 수 체크
+            k += 1
+        cnt = cnt + len(A[j]) - k  # 현재 부분문자 수 + 다음 접미어 길이 - 중복 글자 수  <- j번째 접미어까지의 부분문자 수
+    cnt = cnt - len(A[j]) + k  # j번째 접미어에 찾아야할 부분문자가 있다! -> 일단, j-1번째까지 부분문자 수로 초기화 후 계산
+    print(f'#{tc} {A[j][0]} {k+N-cnt}')
+    # cnt: j-1번째까지의 부분문자수
+    # N: N번째 부분문자는 j번째 접미어에 있다 -> 무조건 cnt보다 크다
+    # k: j번째 접미어에 N번째 부분문자가 있지만, j-1번째까지의 부분문자 중에 중복된 부분문자가 있을 수 있으니 중복값 +
 
 
+
+# ['abac', 'ac', 'bac', 'c']
 '''
 3
 5 abac
