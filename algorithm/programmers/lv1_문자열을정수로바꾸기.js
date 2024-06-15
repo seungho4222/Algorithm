9d4f08f5f45aa3518a3f3109bfd1593ae0d08f20
seungho <kials1@naver.com>
2024-06-15 12:15:28 +0900

    240615

---
+++ algorithm/programmers/lv1_문자열을정수로바꾸기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  return parseInt(s);
+}
+
+solution("1234");
+solution("-1234");
+
+function solution(s) {
+  return Number(s);
+}
+
+function solution(s) {
+  return s / 1;
+}
+
+function solution(s) {
+  return +s;
+}
