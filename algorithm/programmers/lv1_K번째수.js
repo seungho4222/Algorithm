a7f64c6b5050f75bb79f6c6e17901e8b35f25678
seungho <kials1@naver.com>
2024-06-19 23:44:22 +0900

    240619

---
+++ algorithm/programmers/lv1_K번째수.js
@@ -0,0 +1,17 @@
+function solution(array, commands) {
+  return commands.map((command) =>
+    array
+      .slice(command[0] - 1, command[1])
+      .sort((a, b) => a - b)
+      .at(command[2] - 1)
+  );
+}
+
+solution(
+  [1, 5, 2, 6, 3, 7, 4],
+  [
+    [2, 5, 3],
+    [4, 4, 1],
+    [1, 7, 3],
+  ]
+);
