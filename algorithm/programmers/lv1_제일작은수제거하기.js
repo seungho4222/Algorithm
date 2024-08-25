409652de7ee72796212366c7a2c946c4b87238e5
seungho <kials1@naver.com>
2024-08-25 23:11:28 +0900

    240824 - 배열

--- algorithm/programmers/lv1_제일작은수제거하기.js
+++ algorithm/programmers/lv1_제일작은수제거하기.js
@@ -1,6 +1,15 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12935
+ * 프로그래머스 lv1
+ * 제일 작은 수 제거하기
+ * @param {Number[]} arr
+ * @returns {Numberp[]}
+ */
+
 function solution(arr) {
-    const answer = arr.splice(arr.indexOf(Math.min(...arr)),1);
-    return !arr.length ? [-1] : arr;
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  return !arr.length ? [-1] : arr;
 }
 
-console.log(soloution([4,3,2,1]);
+console.log(solution([4, 3, 2, 1]));
+console.log(solution([10]));
